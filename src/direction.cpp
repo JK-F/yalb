794989fbc5f2346c2b18d0a1755ef8af748a2def
myo <>
2026-07-09 20:46:06 +0200

    make direction branchless

--- src/direction.cpp
+++ src/direction.cpp
@@ -1,68 +1,41 @@
 #include "direction.hpp"
 #include <cassert>
-#include <iterator>
 #include <tuple>
 
-const double y_part(Direction direction) {
-  switch (direction) {
-  case UP:
-  case UP_RIGHT:
-  case UP_LEFT:
-    return 1;
-  case DOWN:
-  case DOWN_LEFT:
-  case DOWN_RIGHT:
-    return -1;
-  case NONE:
-  case RIGHT:
-  case LEFT:
-  default:
-    return 0;
-  }
-}
+
+// Look up tables
+static constexpr double x_lut[9] = {0, 1, 0, -1, 0, 1, -1, -1, 1};
+static constexpr double y_lut[9] = {0, 0, 1, 0, -1, 1, 1, -1, -1};
+static constexpr double w_lut[9] = {
+                                    /*NONE*/        4. / 09.,
+                                    /*L,R,U,D*/     1. / 09., 1. / 09., 1. / 09., 1. / 09.,
+                                    /*Diagonal*/    1. / 36., 1. / 36., 1. / 36., 1. / 36., 
+                                    };
 
 const double x_part(Direction direction) {
-  switch (direction) {
-  case RIGHT:
-  case DOWN_RIGHT:
-  case UP_RIGHT:
-    return 1;
-  case LEFT:
-  case DOWN_LEFT:
-  case UP_LEFT:
-    return -1;
-  case NONE:
-  case UP:
-  case DOWN:
-  default:
-    return 0;
-  }
+  return x_lut[direction];
+}
+
+const double y_part(Direction direction) {
+  return y_lut[direction];
 }
 
 std::tuple<int, int> updated_coords(const int &x, const int &y, const Direction &dir, const unsigned int size_x, const unsigned int size_y) {
-  int dx = x_part(dir);
-  int dy = y_part(dir);
-  int new_x = x + dx;
-  int new_y = y + dy;
-  if (new_x >= size_x) {
-    new_x = 0;
-  }
-  else if (new_x < 0) {
-    new_x = size_x - 1;
-  }
-
-  if (new_y >= size_y) {
-    new_y = 0;
-  }
-  else if (dy < 0) {
-    new_y = size_y - 1;
-  }
+  int sx = static_cast<int>(size_x);
+  int sy = static_cast<int>(size_x);
+  int new_x = x + x_part(dir);;
+  int new_y = y + y_part(dir);;
+  // Boolean arithmentic magic
+  // Suggested by Deepseek
+  // It's actually clever
+  new_x -= (new_x >= sx) * sx;   // wraps sx → 0 <--- If new_x < sx it stays, otherwise (new_x = -1) we add sx => -1 + sx  
+  new_x += (new_x < 0)   * sx;   
+  new_y -= (new_y >= sy) * sy;
+  new_y += (new_y < 0)   * sy;
   return std::tuple(new_x, new_y);
 }
 
 const double weight(Direction direction) {
-  double num = direction != 0 ? 1 : 4;
-  double denom = direction <= 4 ? 9 : 36;
-  return num / denom;
+  return w_lut[direction];
 }
 
