8fe3650be92dd888a7ddbce10c244ac5c6a23377
JKF <unijkf@gmail.com>
2026-07-15 15:18:13 +0200

    make direction properly inline

--- src/direction.hpp
+++ src/direction.hpp
@@ -1,23 +1,46 @@
+#include <Kokkos_Core.hpp>
 #include <tuple>
 #define NUM_DIRECTIONS 9
 
 enum Direction {
-  // No Y movement
   NONE = 0,
   RIGHT = 1,
   LEFT = 3,
-  // No X Movement
   UP = 2,
   DOWN = 4,
-  // Diagonals
   UP_RIGHT = 5,
   UP_LEFT = 6,
   DOWN_LEFT = 7,
   DOWN_RIGHT = 8,
 };
 
-double y_part(Direction direction);
-double x_part(Direction direction);
-double weight(Direction direction);
+KOKKOS_INLINE_FUNCTION
+double x_part(Direction direction) {
+  static constexpr double lut[] = {0, 1, 0, -1, 0, 1, -1, -1, 1};
+  return lut[direction];
+}
 
-std::tuple<int, int> updated_coords(const int &x, const int &y, const Direction &dir, const unsigned int size_x, const unsigned int size_y);
+KOKKOS_INLINE_FUNCTION
+double y_part(Direction direction) {
+  static constexpr double lut[] = {0, 0, 1, 0, -1, 1, 1, -1, -1};
+  return lut[direction];
+}
+
+KOKKOS_INLINE_FUNCTION
+double weight(Direction direction) {
+  static constexpr double lut[] = {4. / 9., 1. / 9., 1. / 9., 1. / 9., 1. / 9., 1. / 36., 1. / 36., 1. / 36., 1. / 36.};
+  return lut[direction];
+}
+
+KOKKOS_INLINE_FUNCTION
+std::tuple<int, int> updated_coords(const int &x, const int &y, const Direction &dir, const unsigned int size_x, const unsigned int size_y) {
+  int sx = static_cast<int>(size_x);
+  int sy = static_cast<int>(size_y);
+  int new_x = x + x_part(dir);
+  int new_y = y + y_part(dir);
+  new_x -= (new_x >= sx) * sx;
+  new_x += (new_x < 0)   * sx;
+  new_y -= (new_y >= sy) * sy;
+  new_y += (new_y < 0)   * sy;
+  return std::tuple(new_x, new_y);
+}
